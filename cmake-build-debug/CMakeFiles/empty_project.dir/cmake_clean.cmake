d58db52fcd3d6d3b25908de39185c33c1fad6aff
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2023-11-19 14:25:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/empty_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/empty_project.dir/main.c.o"
+  "empty_project"
+  "empty_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/empty_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
